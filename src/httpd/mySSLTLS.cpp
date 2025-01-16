255cd63c773d224baaeed9430cc729d2d97ad15c
codetoys <codetoys@outlook.com>
2025-01-16 17:19:28 +0800

    太搞笑了，只是需要循环而已，似乎是浏览器不断尝试

--- src/httpd/mySSLTLS.cpp
+++ src/httpd/mySSLTLS.cpp
@@ -25,20 +25,16 @@
 #define CACERT "ca.cer" /*CA 的证书*/
 #define PORT   1111   /*准备绑定的端口*/
 
-#define CHK_NULL(x) if ((x)==NULL) exit (1)
 #define CHK_ERR(err,s) if ((err)==-1) { perror(s); exit(1); }
-#define CHK_SSL(err) if ((err)==-1) { ERR_print_errors_fp(stderr); exit(2); }
 
 int main_SSLTLS()
 {
 	int err;
 	int listen_sd;
-	int sd;
 	struct sockaddr_in sa_serv;
 	struct sockaddr_in sa_cli;
 	socklen_t client_len;
 	SSL_CTX* ctx;
-	SSL* ssl;
 	X509* client_cert;
 	char* str = NULL;
 	char     buf[4096];
@@ -57,17 +53,17 @@ int main_SSLTLS()
 	meth = SSLv23_server_method();  /*采用什么协议(SSLv2/SSLv3/TLSv1)在此指定*/
 
 	ctx = SSL_CTX_new(meth);
-	CHK_NULL(ctx);
+	if (ctx == NULL) exit(1);
 
 	int mode = SSL_VERIFY_NONE;/*验证与否 SSL_VERIFY_NONE SSL_VERIFY_PEER */
 	SSL_CTX_set_verify(ctx, mode, NULL);
 	//if (SSL_VERIFY_PEER == mode)
 	{
-		//if (1 != SSL_CTX_load_verify_locations(ctx, CACERT, NULL)) /*若验证,则放置CA证书*/
-		//{
-		//	ERR_print_errors_fp(stderr);
-		//	exit(2);
-		//}
+		if (1 != SSL_CTX_load_verify_locations(ctx, CACERT, NULL)) /*若验证,则放置CA证书*/
+		{
+			ERR_print_errors_fp(stderr);
+			exit(2);
+		}
 
 		if (1 != SSL_CTX_use_certificate_file(ctx, CERTF, SSL_FILETYPE_PEM))
 		{
@@ -110,61 +106,72 @@ int main_SSLTLS()
 	err = listen(listen_sd, 5);
 	CHK_ERR(err, "listen");
 
-	client_len = sizeof(sa_cli);
-	sd = accept(listen_sd, (struct sockaddr*)(void*)&sa_cli, &client_len);
-	CHK_ERR(sd, "accept");
-	close(listen_sd);
-
-	printf("Connection from %s, port %d\n",
-		inet_ntoa(sa_cli.sin_addr), sa_cli.sin_port);
-
-	/*TCP连接已建立,进行服务端的SSL过程. */
-	printf("Begin server side SSL\n");
-
-	ssl = SSL_new(ctx);
-	CHK_NULL(ssl);
-	//SSL_set_ciphersuites(ssl, "TLS_AES_128_GCM_SHA256");
-	SSL_set_fd(ssl, sd);
-	err = SSL_accept(ssl);
-	printf("SSL_accept finished %d\n",err);
-	CHK_SSL(err);
-
-	/*打印所有加密算法的信息(可选)*/
-	printf("SSL connection using %s\n", SSL_get_cipher(ssl));
-
-	/*得到服务端的证书并打印些信息(可选) */
-	client_cert = SSL_get_peer_certificate(ssl);
-	if (client_cert != NULL)
+	while (true)
 	{
-		printf("Client certificate:\n");
+		int sd;
+		SSL* ssl;
+		
+		client_len = sizeof(sa_cli);
+		sd = accept(listen_sd, (struct sockaddr*)(void*)&sa_cli, &client_len);
+		CHK_ERR(sd, "accept");
 
-		str = X509_NAME_oneline(X509_get_subject_name(client_cert), 0, 0);
-		CHK_NULL(str);
-		printf("\t subject: %s\n", str);
-		free(str);
+		printf("Connection from %s, port %d\n",
+			inet_ntoa(sa_cli.sin_addr), sa_cli.sin_port);
 
-		str = X509_NAME_oneline(X509_get_issuer_name(client_cert), 0, 0);
-		CHK_NULL(str);
-		printf("\t issuer: %s\n", str);
-		free(str);
+		/*TCP连接已建立,进行服务端的SSL过程. */
+		printf("Begin server side SSL\n");
 
-		X509_free(client_cert);/*如不再需要,需将证书释放 */
+		ssl = SSL_new(ctx);
+		if (ssl == NULL) exit(1);
+		//SSL_set_ciphersuites(ssl, "TLS_AES_128_GCM_SHA256");
+		SSL_set_fd(ssl, sd);
+		err = SSL_accept(ssl);
+		printf("SSL_accept finished %d\n", err);
+		if ((err) == -1) { ERR_print_errors_fp(stderr); continue; }
+
+		/*打印所有加密算法的信息(可选)*/
+		printf("SSL connection using %s\n", SSL_get_cipher(ssl));
+
+		/*得到服务端的证书并打印些信息(可选) */
+		client_cert = SSL_get_peer_certificate(ssl);
+		if (client_cert != NULL)
+		{
+			printf("Client certificate:\n");
+
+			str = X509_NAME_oneline(X509_get_subject_name(client_cert), 0, 0);
+			if (str == NULL) exit(1);
+			printf("\t subject: %s\n", str);
+			free(str);
+
+			str = X509_NAME_oneline(X509_get_issuer_name(client_cert), 0, 0);
+			if (str == NULL) exit(1);
+			printf("\t issuer: %s\n", str);
+			free(str);
+
+			X509_free(client_cert);/*如不再需要,需将证书释放 */
+		}
+		else
+			printf("Client does not have certificate.\n");
+
+		/* 数据交换开始,用SSL_write,SSL_read代替write,read */
+		err = SSL_read(ssl, buf, sizeof(buf) - 1);
+		if ((err) == -1) { ERR_print_errors_fp(stderr); continue; }
+		buf[err] = '\0';
+		printf("Got %d chars:'%s'\n", err, buf);
+
+		char outbuf[10240];
+		time_t t1 = time(NULL);
+		sprintf(outbuf, "HTTP/1.0 200 ok\r\nContent-type: text/plain\r\n\r\n%s %d %s request:\r\n%s"
+			, __FILE__, __LINE__, ctime(&t1), buf);
+		err = SSL_write(ssl, outbuf, strlen(outbuf));
+		if ((err) == -1) { ERR_print_errors_fp(stderr); continue; }
+
+		shutdown(sd, 2);
+		SSL_free(ssl);
 	}
-	else
-		printf("Client does not have certificate.\n");
-
-	/* 数据交换开始,用SSL_write,SSL_read代替write,read */
-	err = SSL_read(ssl, buf, sizeof(buf) - 1);
-	CHK_SSL(err);
-	buf[err] = '\0';
-	printf("Got %d chars:'%s'\n", err, buf);
-
-	err = SSL_write(ssl, "I hear you.", strlen("I hear you."));
-	CHK_SSL(err);
 
 	/* 收尾工作*/
-	shutdown(sd, 2);
-	SSL_free(ssl);
+	close(listen_sd);
 	SSL_CTX_free(ctx);
 
 	return 0;
