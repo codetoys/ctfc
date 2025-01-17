b50dc83587bcdc0c9ff0a49e72173b8fa8382b95
codetoys <codetoys@outlook.com>
2025-01-17 17:27:49 +0800

    openssl正确的v3格式证书，服务器证书包含SAN扩展，否则浏览器认为不安全

--- src/httpd/mySSLTLS.h
+++ src/httpd/mySSLTLS.h
@@ -7,32 +7,54 @@
 
 /*
 
+#请求证书 openssl.cnf位置可以通过“openssl version -a”查看（OPENSSLDIR） 必须有SAN，否则新版浏览器不认
+#为了生成v3证书需要通过-extfile传入配置文件，-extensions指示使用配置文件的哪个节
+#注意SAN配置的是域名，可能需要修改hosts文件以便通过域名访问本地服务
+export OPEN_SSL_CONFIG_FILE=/usr/lib/ssl/openssl.cnf
+
 #创建CA证书私钥
 openssl genrsa -aes256 -out ca.key 2048
 #请求证书
 openssl req -new -sha256 -key ca.key -out ca.csr -subj "/C=CN/ST=SD/L=JN/O=QDZY/OU=www.test.com/CN=CA/emailAddress=admin@test.com"
 #自签署证书
-openssl x509 -req -days 36500 -sha256 -extensions v3_ca -signkey ca.key -in ca.csr -out ca.cer
+openssl x509 -req -days 36500 -sha256 -extfile $OPEN_SSL_CONFIG_FILE -extensions v3_ca -signkey ca.key -in ca.csr -out ca.cer
+openssl x509 -text -noout -in ca.cer
 
 #创建服务器私钥
 openssl genrsa -aes256 -out server.key 2048
 #请求证书
-openssl req -new -sha256 -key server.key -out server.csr -subj "/C=CN/ST=SD/L=JN/O=QDZY/OU=www.test.com/CN=SERVER/emailAddress=admin@test.com"
+openssl req -new -sha256 -key server.key -out server.csr -subj "/C=CN/ST=SD/L=JN/O=QDZY/OU=www.test.com/CN=SERVER/emailAddress=admin@test.com" \
+    -reqexts SAN \
+    -config <(cat $OPEN_SSL_CONFIG_FILE <(printf "[SAN]\nsubjectAltName=DNS:www.test.com,DNS:www.test2.com\n"))
+#查看请求
+openssl req  -noout -text -subject -in server.csr
+
 #使用CA证书签署服务器证书
-openssl x509 -req -days 36500 -sha256 -extensions v3_req  -CA  ca.cer -CAkey ca.key  -CAserial ca.srl  -CAcreateserial -in server.csr -out server.cer
+openssl x509 -req -days 36500 -sha256 -extensions v3_req  -CA  ca.cer -CAkey ca.key  -CAserial ca.srl  -CAcreateserial -in server.csr -out server.cer \
+    -extensions SAN \
+    -extfile <(cat $OPEN_SSL_CONFIG_FILE \
+        <(printf "[SAN]\nsubjectAltName=DNS:www.test.com,DNS:www.test2.com\n"))
+#查看证书
+openssl x509 -text -noout -in server.cer
 
 #生成客户端私钥
 openssl genrsa -aes256 -out client.key 2048
 #申请证书
 openssl req -new -sha256 -key client.key  -out client.csr -subj "/C=CN/ST=SD/L=JN/O=QDZY/OU=www.test.com/CN=CLIENT/emailAddress=admin@test.com"
 #使用CA证书签署客户端证书
-openssl x509 -req -days 36500 -sha256 -extensions v3_req  -CA  ca.cer -CAkey ca.key  -CAserial ca.srl  -CAcreateserial -in client.csr -out client.cer
+openssl x509 -req -days 36500 -sha256 -extfile $OPEN_SSL_CONFIG_FILE -extensions v3_req  -CA  ca.cer -CAkey ca.key  -CAserial ca.srl  -CAcreateserial -in client.csr -out client.cer
+openssl x509 -text -noout -in client.cer
 
 #生成用于windows的pfx证书（pem格式无法导入私钥）
 openssl pkcs12 -export -out client.pfx -inkey client.key -in client.cer
 
 #去掉私钥密码
-openssl rsa -in password.key -out nopassword.key
+openssl rsa -in ca.key -out ca.key
+openssl rsa -in server.key -out server.key
+openssl rsa -in client.key -out client.key
+
+#如果提示Can't load /home/user/.rnd into RNG
+openssl rand -writerand /home/user/.rnd
 
 */
 
